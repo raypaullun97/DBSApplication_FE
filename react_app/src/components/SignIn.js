8b9cab3d13408223fc55b268d5473f7ce2be2748
Benjamin Ong <benjamin.ong@trentglobal.edu.sg>
2022-12-03 05:33:41 +0000

    pushing to test login

--- react_app/src/components/SignIn.js
+++ react_app/src/components/SignIn.js
@@ -7,10 +7,15 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import "./SignIn2.css";
 import { Form, Container, Navbar } from 'react-bootstrap';
 import dbsLogo from "../images/DBS-logo.jpg"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+const BASE_URL = "http://172.20.10.2:5000"
 
 const SignIn = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
   useEffect(() => {
@@ -21,31 +26,49 @@ const SignIn = () => {
     if (user) navigate("/TasksPage");
   }, [user, loading]);
 
-  const signin = async () => {
-    console.log("sign in");
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      ); // 2 items are done with this function: 1. Automatically generates a user and saves it in Firebase, 2. Logs the user in
-      console.log(user);
-    } catch (error) {
-      console.log("error at sign in");
-      alert(error.message);
+  // const signin = async () => {
+  //   console.log("sign in");
+  //   try {
+  //     const user = await signInWithEmailAndPassword(
+  //       auth,
+  //       loginEmail,
+  //       loginPassword
+  //     ); // 2 items are done with this function: 1. Automatically generates a user and saves it in Firebase, 2. Logs the user in
+  //     console.log(user);
+  //   } catch (error) {
+  //     console.log("error at sign in");
+  //     alert(error.message);
+  //   }
+  // };
+
+  async function signin() {
+    console.log(loginEmail)
+    console.log(loginPassword)
+
+    if (!loginEmail || !loginPassword) {
+      setLoginFailed(true)
+    } else {
+      const response = await axios.post((BASE_URL + '/account/login'), {
+        "credentials" : {
+          "username" : loginEmail,
+          "password" : loginPassword
+        }
+      })
+
+      console.log(response.data)
     }
-  };
+  }
 
   return (
     <React.Fragment>
       <Navbar bg="dark">
         
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="#home" className='mx-2'>
             <img
               src={dbsLogo}
-              width="45"
-              height="30"
-              className="d-inline-block align-top"
+              width="75"
+              height="50"
+              className="d-inline-block align-top mx-2"
               alt="dbsLogo"
             />
           </Navbar.Brand>
@@ -70,7 +93,7 @@ const SignIn = () => {
             />
 
             <Link>
-              <button className="signInButton" onClick={signin}>
+              <button className="signInButton button" onClick={signin}>
                 Sign In
               </button>
             </Link>
@@ -82,6 +105,7 @@ const SignIn = () => {
             </div>
           </div>
         </div>
+        
 
     </React.Fragment>
 
