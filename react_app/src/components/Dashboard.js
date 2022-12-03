4577692c695c4c2523c2da6d5f01500e4225964c
Benjamin Toh <78788899+kwaraa@users.noreply.github.com>
2022-12-03 14:40:04 +0800

    Added Dashboard template

---
+++ react_app/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavBar from "./NavBar"
+import "./Dashboard.css"
+
+const Dashboard = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div>
+            <button classname="button1">
+                Testing
+            </button>
+            <button>
+                Testing
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
